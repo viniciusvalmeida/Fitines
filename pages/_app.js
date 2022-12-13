97f3a2eb265bab24d2cafcf190c477fbea8c4a27
Vinícius Almeida <81371812+viniciusvalmeida@users.noreply.github.com>
2022-12-13 15:27:11 -0300

    WIT: Sync Function in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/globals.css'
 
+(async () => {
+  const sequelize = require('../config/sequelize')
+
+  sequelize.sync()
+})()
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
