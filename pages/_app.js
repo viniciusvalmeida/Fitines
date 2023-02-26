fa3fdfaa9fa1366449df65e0a5fa7fc1113cff0f
viniciusvalmeida <81371812+viniciusvalmeida@users.noreply.github.com>
2023-02-26 12:02:27 -0300

    IMPORT BOOTSTRAP AND REACSTRAP

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { useEffect } from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Layout from "./src/components/Layout";
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    require('bootstrap/dist/js/bootstrap.bundle.min.js')
-  }, [])
-  
-  return <Component {...pageProps} />
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    );
 }
 
-export default MyApp
+export default MyApp;
