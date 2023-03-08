35ff26bce09c674f3a7930c1c2c59c9bdadc5b88
Vinícius Almeida <81371812+viniciusvalmeida@users.noreply.github.com>
2023-03-08 15:20:12 -0300

    REMOVED UNNECESSARY MAIN TAGS

--- pages/carrinho.js
+++ pages/carrinho.js
@@ -1,3 +1,20 @@
-export default function Carrinho() {
-  return <h1>CARRINHO</h1>
-}
+import Head from "next/head";
+import { Container } from "reactstrap";
+
+const Carrinho = () => {
+    return (
+        <>
+            <Head>
+                <title>Carrinho</title>
+                <meta name="description" content="Meu carrinho de compras" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <Container className="mb-5">
+                <h1 className="my-5">Carrinho</h1>
+            </Container>
+        </>
+    );
+};
+
+export default Carrinho;
--- pages/produtos.js
+++ pages/produtos.js
@@ -4,36 +4,35 @@
 
 // pages/products.tsx
 
-import Head from 'next/head'
-import { Container } from 'reactstrap'
-import ProdutosList from './src/components/ProdutosList'
-import { fetchProdutos } from './src/services/produtos'
+import Head from "next/head";
+import { Container } from "reactstrap";
+import ProdutosList from "./src/components/ProdutosList";
+import { fetchProdutos } from "./src/services/produtos";
 
 export const getStaticProps = async () => {
-  const products = await fetchProdutos()
-  return { props: { products } }
-}
+    const products = await fetchProdutos();
+    return { props: { products } };
+};
 
 const Products = (props) => {
-  return (
-    <>
-      <Head>
-        <title>Nossos Produtos</title>
-        <meta name="description" content="Conheça todos os nossos produtos" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <>
+            <Head>
+                <title>Nossos Produtos</title>
+                <meta
+                    name="description"
+                    content="Conheça todos os nossos produtos"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main>
-        <Container className="mb-5">
-          <h1 className="my-5">
-            Nossos Produtos
-          </h1>
+            <Container className="mb-5">
+                <h1 className="my-5">Nossos Produtos</h1>
 
-          {<ProdutosList products={props.products} />}
-        </Container>
-      </main>
-    </>
-  )
-}
+                {<ProdutosList products={props.products} />}
+            </Container>
+        </>
+    );
+};
 
-export default Products
+export default Products;
