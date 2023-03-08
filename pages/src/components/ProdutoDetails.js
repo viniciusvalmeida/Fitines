8924cec63c8f0d85b3e731f907c414ea942bf8d0
Vinícius Almeida <81371812+viniciusvalmeida@users.noreply.github.com>
2023-03-08 15:04:59 -0300

    FIXED: TOAST AT PRODUTO DETAILS OK

--- pages/src/components/ProdutoDetails.js
+++ pages/src/components/ProdutoDetails.js
@@ -4,40 +4,47 @@ import { Button, Col, Row } from "reactstrap";
 import SuccessToast from "./SuccessToast";
 
 const ProductDetails = ({ product }) => {
-  const [toastIsOpen, setToastIsOpen] = useState(false)
+    const [toastIsOpen, setToastIsOpen] = useState(false);
 
-  return (
-    <Row>
-      <Col lg={6}>
-        <Image
-          src={`/produtos/${product.Imagem}`}
-          alt={product.Nome}
-          height={500}
-          width={600}
-        />
-      </Col>
+    return (
+        <Row>
+            <Col lg={6}>
+                <Image
+                    src={`/produtos/${product.Imagem}`}
+                    alt={product.Nome}
+                    height={500}
+                    width={600}
+                />
+            </Col>
 
-      <Col lg={6}>
-        <h1>{product.Nome}</h1>
+            <Col lg={6}>
+                <h1>{product.Nome}</h1>
 
-        <h2 className="text-muted">R$ {product.Preco}</h2>
+                <h2 className="text-muted">R$ {product.Preco}</h2>
 
-        <p className="my-3">
-          <span className="d-block font-weight-bold">Descrição:</span>
-          {product.Descricao}
-        </p>
+                <p className="my-3">
+                    <span className="d-block font-weight-bold">Descrição:</span>
+                    {product.Descricao}
+                </p>
 
-        <Button
-          color="dark"
-          className="my-3 pb-2"
-        >
-          Compre agora
-        </Button>
+                <Button 
+                  color="dark" 
+                  className="my-3 pb-2"
+                  onClick={() => {
+                    setToastIsOpen(true);
+                    setTimeout(() => setToastIsOpen(false), 1000 * 3);
+                  }}
+                >
+                    Compre agora
+                </Button>
 
-        <SuccessToast toastIsOpen={toastIsOpen} setToastIsOpen={setToastIsOpen} />
-      </Col>
-    </Row>
-  )
-}
+                <SuccessToast
+                    toastIsOpen={toastIsOpen}
+                    setToastIsOpen={setToastIsOpen}
+                />
+            </Col>
+        </Row>
+    );
+};
 
-export default ProductDetails
+export default ProductDetails;
