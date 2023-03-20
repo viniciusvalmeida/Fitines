81c68ef56819f30af02c6e240ee66a5c57b572e0
Vinícius Almeida <81371812+viniciusvalmeida@users.noreply.github.com>
2023-03-20 15:06:01 -0300

    ADD TRY CATCH TO PRODUTOS FETCH

--- pages/src/services/produtos.js
+++ pages/src/services/produtos.js
@@ -1,23 +1,33 @@
  const hostAddress = process.env.NEXT_PUBLIC_HOST_ADDRESS
  
  export const fetchProdutos = async () => {
-  const produtos = await fetch(`${hostAddress}/api/produtos`,{
-    Headers: {
-    "Content-Type": "application/json"
+  try {
+    const produtos = await fetch(`${hostAddress}/api/produtos`,{
+      Headers: {
+      "Content-Type": "application/json"
+    }
+    })
+    .then(res => res.json())
+  
+    return produtos
+    
+  } catch (e) {
+    console.log(e)
   }
-  })
-  .then(res => res.json())
-
-  return produtos
 }
 
 export const fetchProduto = async (id) => {
-  const produto = await fetch(`${hostAddress}/api/produtos/${id}`,{
-    Headers: {
-    "Content-Type": "application/json"
+  try {
+    const produto = await fetch(`${hostAddress}/api/produtos/${id}`,{
+      Headers: {
+      "Content-Type": "application/json"
+    }
+    }).then(res => res.json())
+  
+    return produto
+    
+  } catch (e) {
+    console.log(e);
   }
-  }).then(res => res.json())
-
-  return produto
 }
 
