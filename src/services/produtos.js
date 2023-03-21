2ccc9327bef5a6a732e41ecae1b3688f0be75b31
viniciusvalmeida <81371812+viniciusvalmeida@users.noreply.github.com>
2023-03-21 17:32:31 -0300

    WIT getStaticPath FETCH

--- src/services/produtos.js
+++ src/services/produtos.js
@@ -1,33 +1,49 @@
+ import db from "../../models";
+ 
  const hostAddress = process.env.NEXT_PUBLIC_HOST_ADDRESS
  
  export const fetchProdutos = async () => {
-  try {
-    const response = await fetch(`${hostAddress}/api/produtos`,{
-      Headers: {
-      "Content-Type": "application/json"
-    }
-    })
-    const produtos = await response.json()
-  
-    return produtos
     
-  } catch (e) {
-    console.log({error: e})
-  }
+  const listaProdutos = await db.produtos.findAll({
+    attributes: ['id', 'Nome', 'Preco', 'Imagem'],
+    include: [
+      {
+        model: db.tamanhos,
+        attributes: ['Nome']
+      },
+      {
+        model: db.sexo,
+        attributes: ['Sexo']
+      },
+      {
+        model: db.categorias,
+        attributes: ['Nome']
+      }
+    ]
+  })
+    
+    return listaProdutos.map(prod => prod.toJSON())
 }
 
 export const fetchProduto = async (id) => {
-  try {
-    const produto = await fetch(`${hostAddress}/api/produtos/${id}`,{
-      Headers: {
-      "Content-Type": "application/json"
-    }
-    }).then(res => res.json())
-  
-    return produto
-    
-  } catch (e) {
-    console.log({error: e});
-  }
+  const produto = await db.produtos.findByPk(id, {
+    attributes: ['id', 'Nome', 'Preco', 'Imagem', 'Descricao'],
+      include: [
+        {
+          model: db.tamanhos,
+          attributes: ['Nome']
+        },
+        {
+          model: db.sexo,
+          attributes: ['Sexo']
+        },
+        {
+          model: db.categorias,
+          attributes: ['Nome']
+        }
+      ]
+  })
+
+  return produto.toJSON()
 }
 
