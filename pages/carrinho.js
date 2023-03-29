9c4b12c7596d0e53231318585c041feed12cb202
Vinícius Almeida <81371812+viniciusvalmeida@users.noreply.github.com>
2023-03-29 15:08:13 -0300

    WIT: Carrinho table

--- pages/carrinho.js
+++ pages/carrinho.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { Container } from "reactstrap";
+import { Container, Input, Table } from "reactstrap";
 
 const Carrinho = () => {
     return (
@@ -12,6 +12,37 @@ const Carrinho = () => {
 
             <Container className="mb-5">
                 <h1 className="my-5">Carrinho</h1>
+
+                <Table className="mb-5 shadow-lg rounded-3 table-hover text-center">
+                    
+                    <thead>
+                        <tr className="table-dark">
+                            <th>Produtos</th>
+                            <th>Und</th>
+                            <th>Sub-total</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td>Produto1</td>
+                            <td>
+                                <Input></Input>
+                            </td>
+                            <td>R$50</td>
+                        </tr>
+                        <tr>
+                            <td>Produto2</td>
+                            <td>1</td>
+                            <td>R$25.99</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td colSpan={3} className="fw-bold">TOTAL: R$75.99</td>
+                        </tr>
+                    </tbody>
+                </Table>
             </Container>
         </>
     );
