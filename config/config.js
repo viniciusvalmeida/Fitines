b3bd999aa941b66aeca2d0899a9987494180d8a4
Vinícius Almeida <81371812+viniciusvalmeida@users.noreply.github.com>
2023-04-10 10:37:38 -0300

    DIALECT MODULE ADD

--- config/config.js
+++ config/config.js
@@ -2,6 +2,7 @@ require('dotenv').config({ path: './.env.local' })
 
 module.exports = {
     dialect: process.env.NEXT_PUBLIC_DB_DIALECT,
+    dialectModule: require('pg'),
     host: process.env.NEXT_PUBLIC_DB_HOST,
     username: process.env.NEXT_PUBLIC_DB_USER,
     password: process.env.NEXT_PUBLIC_DB_PASSWORD,
