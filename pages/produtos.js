dcddf2e6dac4cbbafbcd4f519c1d788b34ab863e
Vinícius Almeida <81371812+viniciusvalmeida@users.noreply.github.com>
2023-04-12 08:45:57 -0300

    ADD HEADER TO PRODUTOS AND ID

--- pages/produtos.js
+++ pages/produtos.js
@@ -13,20 +13,6 @@ const Products = (props) => {
     return (
         <>
             <Head>
-                <title>Produtos</title>
-                <meta name="description" content="Produtos FitInês" />
-                <meta charSet="utf-8"></meta>
-                <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-                <link rel="manifest" href="/site.webmanifest"/>
-                <link rel="icon" href={`${process.env.NEXT_PUBLIC_HOST_ADDRESS}/favicon.ico?v=2`} />
-            </Head>
-
-            <Header />
-
-            <Head>
                 <title>Nossos Produtos</title>
                 <meta
                     name="description"
@@ -34,6 +20,8 @@ const Products = (props) => {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            
+            <Header />
 
             <Container className="mb-5">
                 <h1 className="my-5">Nossos Produtos</h1>
--- pages/produtos/[id].js
+++ pages/produtos/[id].js
@@ -29,6 +29,18 @@ export const getStaticPaths = async () => {
 const Product = (props) => {
     return (
         <>
+            <Head>
+                <title>{props.product.Nome}</title>
+                <meta name="description" content="Produtos FitInês" />
+                <meta charSet="utf-8"></meta>
+                <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                <link rel="manifest" href="/site.webmanifest"/>
+                <link rel="icon" href={`${process.env.NEXT_PUBLIC_HOST_ADDRESS}/favicon.ico?v=2`} />
+            </Head>
+
             <Header />
 
             <div>
