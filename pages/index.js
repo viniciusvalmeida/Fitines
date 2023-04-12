ce8d1e2b54800f4389526c2579e0a4071fcb073b
Vinícius Almeida <81371812+viniciusvalmeida@users.noreply.github.com>
2023-04-12 08:40:03 -0300

    ADD HEADER TO PAGES

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
 import Link from "next/link";
 import Header from "../src/components/Header";
 import { Button, Container } from "reactstrap";
+import Head from "next/head";
 
 export default function Home() {
     return (
         <>
+            <Head>
+                <title>FitInês</title>
+                <meta charSet="utf-8"></meta>
+                <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                <link rel="manifest" href="/site.webmanifest"/>
+                <link rel="icon" href={`${process.env.NEXT_PUBLIC_HOST_ADDRESS}/favicon.ico?v=2`} />
+            </Head>
+            
             <Header />
 
             <Container className="py-5 text-center">
